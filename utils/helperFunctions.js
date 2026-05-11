83af6e2215c40ccaf9094bcc0fe669413c77f72f
davidy <david.yang026@gmail.com>
2026-05-11 15:27:42 -0700

    updated system prompt to refine response

--- utils/helperFunctions.js
+++ utils/helperFunctions.js
@@ -8,14 +8,12 @@ export async function getLocation() {
   }
 }
 
-export async function getWeatherByCity({location}) {
-  console.log("this is the city", location)
+export async function getWeatherByCity({ location }) {
   // Geocoding—extract lat and lon
   const geoRes = await fetch(
     `https://geocoding-api.open-meteo.com/v1/search?name=${encodeURIComponent(location)}&count=1`,
   );
   const geoData = await geoRes.json();
-  console.log("this is geoData", geoData)
 
   const { latitude, longitude, name } = geoData.results[0];
 
@@ -24,9 +22,7 @@ export async function getWeatherByCity({location}) {
     `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current_weather=true&temperature_unit=fahrenheit`,
   );
   const weatherData = await weatherRes.json();
-
-  return weatherData.current_weather.temperature
-
+  return weatherData.current_weather.temperature;
 }
 
 export const tools = [
@@ -34,7 +30,8 @@ export const tools = [
     type: "function",
     function: {
       name: "getWeatherByCity",
-      description: "Get the current weather for user's location and return the temperature in fareinheit",
+      description:
+        "Get the current weather for user's location and return the temperature in fareinheit",
       parameters: {
         type: "object",
         properties: {
