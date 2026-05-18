e0959d3903925dc05c1ec48fd0142369f20e5ffd
davidy <david.yang026@gmail.com>
2026-05-18 10:21:10 -0700

    updated rate limit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.get(/^(?!\/api).*/, (req, res) => {
 
 const limiter = rateLimit({
   windowMs: 1*60 *1000, 
-  limit: 2,
+  limit: 5,
   standardHeaders: 'draft-8',
   legacyHeaders:false,
   ipv6Subnet:56,
